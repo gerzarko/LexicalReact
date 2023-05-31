2d4dba1ba6764a1b6bd9f12bb220338edd11ea80
ger <68071939+gerzarko@users.noreply.github.com>
2023-05-31 17:35:27 -0300

    text editor working

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Editor } from "./components/Editor";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="m-10">Text Editor</h1>
+        <Editor />
+      </div>
+    </>
+  );
+}
+
+export default App;
