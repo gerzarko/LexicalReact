cc746962db6044fda27fc319b4919e01dcb82bcd
ger <68071939+gerzarko@users.noreply.github.com>
2023-06-20 23:05:11 -0400

    clean some of the functions

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -27,36 +27,23 @@ import {
 } from "lexical";
 import { $setBlocksType } from "@lexical/selection";
 
-// When the editor changes, you can get notified via the
-// LexicalOnChangePlugin!
 function onChange(editorState: EditorState) {
   editorState.read(() => {
-    // Read the contents of the EditorState here.
     const root = $getRoot();
     const selection = $getSelection();
-
-    // console.log(root, selection);
   });
 }
 
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
 function MyCustomAutoFocusPlugin() {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
-    // Focus the editor when the effect fires!
     editor.focus();
   }, [editor]);
 
   return null;
 }
 
-// Catch any errors that occur during Lexical updates and log them
-// or throw them as needed. If you don't throw them, Lexical will
-// try to recover gracefully without losing user data.
 function onError(error: Error) {
   console.error(error);
 }
@@ -89,25 +76,6 @@ export function Editor() {
     return null;
   }
 
-  function OtherCustomPlugin(props: {
-    onChange: (editorState: EditorState) => void;
-  }) {
-    const [editor] = useLexicalComposerContext();
-    setTimeout(() => {
-      console.log(editor);
-    }, 2000);
-
-    return null;
-  }
-  // function AddConsoleLogPlugIn(): JSX.Element {
-  //     const [editor] = useLexicalComposerContext();
-  //     const mostrarEstadoComposer = (e: any) => {
-  //       console.log(editor);
-  //     };
-  //     return <button onClick={mostrarEstadoComposer}></button>;
-  //   }
-  // }
-
   function ShowEditorCOmposerState(): JSX.Element {
     const [editor] = useLexicalComposerContext();
     const mostrarEstadoComposer = () => {
@@ -126,14 +94,6 @@ export function Editor() {
 
     return <button onClick={appendH1}>h1</button>;
   }
-
-  // Inside the `editor.update` you can use special $ prefixed helper functions.
-  // These functions cannot be used outside the closure, and will error if you try.
-  // (If you're familiar with React, you can imagine these to be a bit like using a hook
-  // outside of a React function component).
-  // editor.update(() => {
-  // Get the RootNode from the EditorState
-
   function AddText(): JSX.Element {
     const [editor] = useLexicalComposerContext();
     const addHola = () => {
@@ -158,12 +118,6 @@ export function Editor() {
         const root = $getRoot();
         const selection = $getSelection();
         removeNode(selection, false, false);
-
-        // if ($isRangeSelection(selection)) {
-        //   $setBlocksType(selection, () =>
-
-        //   );
-        // }
       });
     };
     return <button onClick={addHola}>Chaur</button>;
@@ -228,13 +182,9 @@ export function Editor() {
 
       if (nodesText.find(isText)) {
         const nodee: object[] = nodesText.filter(isText);
-        console.log("---------");
 
         selectionNode(nodee);
-        console.log("---------");
-        console.log(nodee);
       }
-      console.log(searchText);
     }
 
     return (
@@ -254,73 +204,6 @@ export function Editor() {
     );
   }
 
-  function SearchText(): JSX.Element {
-    const [editor] = useLexicalComposerContext();
-    const [searchText, setSearchText] = useState("");
-    let nodesText: object[] = [];
-    const [nodeFound, setNodeFound] = useState([]);
-
-    function all() {
-      editor.update(() => {
-        const root = $getRoot();
-        const all = root.getAllTextNodes();
-        const text: object[] = all.map((node) => {
-          return { text: node.__text, key: node.__key };
-        });
-        nodesText = text;
-        console.log(text);
-      });
-    }
-
-    function selectionNode(nodos: any) {
-      editor.update(() => {
-        const rangeSelection = $createRangeSelection();
-        const root = $getRoot();
-        $setSelection(rangeSelection);
-
-        const someNode = $getNodeByKey(nodos[0].key);
-        someNode!.select();
-        someNode?.remove(true);
-
-        console.log(someNode, "nodo");
-      });
-    }
-    function handleChange(event: any) {
-      setSearchText(event.target.value);
-    }
-
-    function isText(text: any) {
-      return text.text === searchText;
-    }
-
-    function handleSubmit(event: any) {
-      event.preventDefault();
-      all();
-
-      if (nodesText.find(isText)) {
-        const nodee: object[] = nodesText.filter(isText);
-        nodee.map((nodo) => {
-          return console.log(nodo);
-        });
-        console.log("---------");
-
-        selectionNode(nodee);
-        console.log("---------");
-        console.log(nodee);
-      }
-      console.log(searchText);
-    }
-
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input onChange={handleChange} type="text" id="searchText"></input>
-          <input type="submit" value="submit" />
-        </form>
-      </div>
-    );
-  }
-
   function AllTextNode(): JSX.Element {
     const [editor] = useLexicalComposerContext();
     function all() {
@@ -333,27 +216,13 @@ export function Editor() {
 
     return <button onClick={all}>Show TextNodes</button>;
   }
-
-  function RegularExpression(): JSX.Element {
-    function check() {
-      const regular = /n/g;
-      const aCheaquear = new RegExp("n", "");
-      const variable = "hola andaana".replace(regular, "chau");
-      console.log(variable);
-    }
-
-    return <button onClick={check}>regular expression</button>;
-  }
-
   return (
     <LexicalComposer initialConfig={initialConfig}>
       <ShowEditorCOmposerState />
       {/* <AddH1 /> */}
       {/* <AddText /> */}
       {/* <DeleteText /> */}
-      <SearchText />
       <SearchAndReplace />
-      <RegularExpression />
 
       <AllTextNode />
       <PlainTextPlugin
